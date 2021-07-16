c2522a0fa6f8215efc81ca70b89cc1c436ac0ae4
Jadeclon <41341683+Jadeclon@users.noreply.github.com>
2021-07-16 18:38:36 +0200

    Add files via upload

--- lagerwelt.py
+++ lagerwelt.py
@@ -9,6 +9,7 @@ import mysql.connector
 from mysql.connector import Error
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 # streamlit run "C:/Users/jadec/OneDrive/Workspaces/pythonWorkspace/lagerwelt.py"
 
@@ -25,7 +26,7 @@ st.write("""
 articles = pd.DataFrame()
 
 def search(articleNr):
-    result = articles[5].str.match(articleNr)
+    result = articles['Artikelnummer'].str.match(articleNr)
     table.write(articles[result])
 
 
@@ -34,8 +35,85 @@ articles = []
 
 table = st.empty()
 
+
+# =============================================================================
+# Brands Pie Chart
+# =============================================================================
+def getBrandsChart():
+    result = articles['Marke'].str.contains('Mercedes')
+    # global mercedeses
+    mercedeses = articles[result]
+    mercedeses['Marke'] = 'Mercedes-Benz'
+    
+    result = articles['Marke'].str.contains('Volkswagen|VW')
+    vws = articles[result]
+    vws['Marke'] = 'Volkswagen'
+    
+    result = articles['Marke'].str.contains('BMW')
+    bmws = articles[result]
+    bmws['Marke'] = 'BMW'
+    
+    result = articles['Marke'].str.contains('Opel')
+    opel = articles[result]
+    opel['Marke'] = 'Opel'
+    
+    result = articles['Marke'].str.contains('Volkswagen|VW|Mercedes|BMW|Opel')
+    # global andere
+    andere = articles[result == False]
+    andere['Marke'] = 'Andere'
+    
+    # global marken
+    marken = vws
+    marken = marken.append(mercedeses)
+    marken = marken.append(bmws)
+    marken = marken.append(opel)
+    marken = marken.append(andere)
+    
+    fig = px.pie(marken, values="Anzahl", names='Marke', title='Marke')
+    st.write(fig)
+
+
+
+def getManufacturerChart():
+    manufacturerList = ['Bosch', 'Valeo', 'MD', 'Denso']
+    result = articles['Hersteller'].str.contains(manufacturerList[0])
+    bosch = articles[result]
+    bosch['Hersteller'] = 'Bosch' # Alle einheitlich benennen
+    
+    result = articles['Hersteller'].str.contains(manufacturerList[1])
+    valeo = articles[result]
+    valeo['Hersteller'] = 'Valeo' # Alle einheitlich benennen
+    
+    result = articles['Hersteller'].str.contains(manufacturerList[2])
+    md = articles[result]
+    md['Hersteller'] = 'MD-Teile' # Alle einheitlich benennen
+    
+    result = articles['Hersteller'].str.contains(manufacturerList[3])
+    denso = articles[result]
+    denso['Hersteller'] = 'Denso' # Alle einheitlich benennen
+    
+    result = articles['Hersteller'].str.contains('Bosch|Valeo|MD|Denso')
+    andere = articles[result == False]
+    andere['Hersteller'] = 'Andere' # Alle einheitlich benennen
+    
+    manufacturer = pd.DataFrame()
+    manufacturer = manufacturer.append(bosch)
+    manufacturer = manufacturer.append(valeo)
+    manufacturer = manufacturer.append(md)
+    manufacturer = manufacturer.append(denso)
+    manufacturer = manufacturer.append(andere)
+    
+    fig = px.pie(manufacturer, values='Anzahl', names='Hersteller', title='Hersteller')
+    st.write(fig)
+
+
+
+# =============================================================================
+# Get articles table
+# =============================================================================
 def connect():
     global articles
+    global result
     try:
         connection = mysql.connector.connect(host='sql150.main-hosting.eu',
                                              database='u794832960_lagerwelt',
@@ -60,14 +138,41 @@ def connect():
                     
                     
             articles = pd.DataFrame(articles)
-            # articles.columns = ["id", "username", "password", "level", "prefix", "firstname", "lastname"]
+            articles.columns = ["artikel_Id", "Marke", "Ampere", "ArtikelArt", "Lagerplatz", "Artikelnummer", "Anzahl", "Hersteller", "Zustand", "OE", "EbayPlus", "webshop", "URL", "proofed", "vehicleId", "oeId"]
+            articles = articles.drop(columns=['artikel_Id', 'proofed', 'vehicleId', 'oeId', 'webshop', 'URL'])
             table.write(articles)
-            bosch = articles[7].str.match('Bosch')
-            st.write('Bosch Aggregate: ' + str( bosch.count() ))
+            
+            fig = px.pie(articles, values="Anzahl", names='ArtikelArt', title='Artikel Typen')
+            st.write(fig)
+            
+
+            
+            getBrandsChart()
+            getManufacturerChart()
+            
+            
+
+            
+            
+            # result = articles['Marke'].str.contains('VW')
+            # result = result.append( articles['Marke'].str.contains('Mercedes') )
+            # global vws
+            # vws = articles[result]
+            # mercedeses['Marke'] = 'Mercedes-Benz'
+            # fig = px.pie(articles['Marke'], values="Anzahl", names='Marke', title='Marke')
+            # st.write(fig)
+            
+            
+            # bosch = articles[7].str.match('Bosch')
+            # st.write('Bosch Aggregate: ' + str( bosch.count() ))
     except Error as e:
         print("Error while connecting to MySQL", e)
 
 
+
+# =============================================================================
+# Get benutzer table
+# =============================================================================
 def connect3():
     global articles
     try:
@@ -94,7 +199,7 @@ def connect3():
                     
                     
             articles = pd.DataFrame(articles)
-            articles.columns = ["id", "username", "password", "level", "prefix", "firstname", "lastname"]
+            # benutzer.columns = ["id", "username", "password", "level", "prefix", "firstname", "lastname"]
             st.write(articles)
     except Error as e:
         print("Error while connecting to MySQL", e)
