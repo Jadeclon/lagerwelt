381794b38af09a221b1c85870320f9d4b3c3087e
Jadeclon <jadeclon@hotmail.de>
2021-08-26 15:33:32 +0200

    make it better

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ app.put('/api/update', (req, res) => {
 });
 
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
       console.log(`Running on port ${PORT}...`);
 });
